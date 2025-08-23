2499c6e976e7401839055069ca4414c1d03c230f
Abhishek kumar Mishra <mkabhi2@gmail.com>
2025-08-23 11:53:52 +0530

    Update Trie.java

--- Trie.java
+++ Trie.java
@@ -17,30 +17,29 @@ public class Trie {
 
     public void insert(String newWord){
         TrieNode curr = root;
-        for(char c: newWord.toCharArray()){
-            if(curr.children.containsKey(c)){
-                curr = curr.children.get(c);
+        for(char c: newWord.toCharArray()){       //Iterate through all the characters of the String
+            if(curr.children.containsKey(c)){     // If the current character is current nodes child
+                curr = curr.children.get(c);       // Make the current node = child node and move to next character
             } else {
-                TrieNode newNode = new TrieNode();
-                curr.children.put(c, newNode);
-                curr = newNode;
+                TrieNode newNode = new TrieNode(); //If the current character is not current nodes child
+                curr.children.put(c, newNode);      //Make a new Node
+                curr = newNode;                     //Make the current node = new Node and move to next character
             }
         }
-
-        curr.isEndOfWord = true;
+        curr.isEndOfWord = true;                    //Once all characters of String are finished, marked the current Node as an endOfWord.
     }
 
     public Optional<TrieNode> search(String searchKey){
         TrieNode current = root;
-        for(char c: searchKey.toCharArray()){
-            if(current.children.containsKey(c)){
-                current = current.children.get(c);
-            } else {
-                return Optional.empty();
+        for(char c: searchKey.toCharArray()){       //Iterate through all the characters of the String
+            if(current.children.containsKey(c)){    // If the current character is current nodes child
+                current = current.children.get(c);  // Make the current node = child node and move to next character
+            } else {                                //If the current character is not current nodes child
+                return Optional.empty();            //Return as the word is not present in the trie
             }
         }
         if(current.isEndOfWord){
-            return Optional.of(current);
+            return Optional.of(current);            //Checks if after all characters matched, if the String is complete
         }
         return Optional.empty();
     }
